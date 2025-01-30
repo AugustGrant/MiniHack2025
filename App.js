6129dbd19b3a27d199bc8c26d5b9d901bbdfdbdb
jennaesx <jennaessex02@gmail.com>
2025-01-30 18:44:11 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          WELCOME TO ECO-SNAPS
+        </p>
+        <button className="submit-button">Submit Your Sustainability Picture Here</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
